84199e46ec242ddbda6320841b9e0865abdbb527
Genaro Gonzalez <genarrogg@gmail.com>
2020-02-01 14:02:21 -0400

    Components y sus rutas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 /* Redux */
-import { Provider } from "react-redux"
-import store from "./store"
-
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Provider store={store}>
-      <p>crud</p>
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Productos} />
+            <Route exact path="/productos/nuevo" component={NuevoProducto} />
+            <Route exact path="/productos/:id" component={EditarProducto} />
+          </Switch>
+        </div>
+      </Provider>
+    </Router>
   );
 }
 
