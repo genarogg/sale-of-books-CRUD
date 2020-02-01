1244858eda103f2f87dec90bf261f220883935c5
Genaro Gonzalez <genarrogg@gmail.com>
2020-02-01 15:34:10 -0400

    gregando el Formulario y Listado de Productos p1

--- src/componentes/NuevoProducto.js
+++ src/componentes/NuevoProducto.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const NuevoProducto = () => {
-    return(
-        <p>Productos ...</p>
-    )
-}
+  return (
+    <div className="row justify-content-center mt-5">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold ">
+              Agregar Nuevo Libro
+            </h2>
+            <form>
+              <div className="form-group">
+                <label>Nombre Libro</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre Libro"
+                />
+              </div>
+              <div className="form-group">
+                <label>Precio Libro</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Precio Libro"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Agregar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default NuevoProducto;
