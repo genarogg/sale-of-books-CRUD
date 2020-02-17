04cdf56a2c5c68ea85afa2e25649872c6426603f
Genaro Gonzalez <genarrogg@gmail.com>
2020-02-17 11:57:55 -0400

    sweetalert2

--- src/componentes/Producto.js
+++ src/componentes/Producto.js
@@ -1,37 +1,58 @@
 import React from "react";
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 /* Redux */
-import { useDispatch } from "react-redux"
-import { borrarProductoAction } from "../actions/productosActions"
+import { useDispatch } from "react-redux";
+import { borrarProductoAction } from "../actions/productosActions";
 
-const Producto = ({producto}) => {
+const Producto = ({ producto }) => {
+  /* dispatch para ejecutar las acciones */
+  const dispatch = useDispatch();
 
-    /* dispatch para ejecutar las acciones */
-    const dispatch = useDispatch()
-
-    const confirmarEliminacionProducto = id => {
-        console.log(id)
-        dispatch(borrarProductoAction())
-    }
-    return(
-        <tr>
-            <td>{producto.nombre}</td>
-            <td><span className="font-weight-bold">${producto.precio}</span></td>
-            <td className="acciones">
-                <Link to={`/productos/editar/${producto.id}`}
-                        className="btn btn-primary mr-2"
-                >
-
-                Editar
-                </Link>
-                <button 
-                    className="btn btn-danger"
-                    onClick={() => confirmarEliminacionProducto(producto.id)}
-                >Eliminar</button>
-            </td>
-        </tr>
-    )
-}
+  const confirmarEliminarProducto = id => {
+    /* Preguntar al usuario */
+    Swal.fire({
+      title: "¿Estás seguro?",
+      text: "Un producto eliminado no se puede recuperar",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "SI, Eliminar!",
+      cancelButtonText: "Cancelar"
+    }).then(result => {
+      if (result.value) {
+        Swal.fire(
+          "Eliminado",
+          "El producto se eliminó correctamente",
+          "success"
+        );
+        dispatch(borrarProductoAction(id));
+      }
+    });
+  };
+  return (
+    <tr>
+      <td>{producto.nombre}</td>
+      <td>
+        <span className="font-weight-bold">${producto.precio}</span>
+      </td>
+      <td className="acciones">
+        <Link
+          to={`/productos/editar/${producto.id}`}
+          className="btn btn-primary mr-2"
+        >
+          Editar
+        </Link>
+        <button
+          className="btn btn-danger"
+          onClick={() => confirmarEliminarProducto(producto.id)}
+        >
+          Eliminar
+        </button>
+      </td>
+    </tr>
+  );
+};
 
 export default Producto;
