0d2b6da40961c42b2a9ff9cc039319ceb6774688
Genaro Gonzalez <genarrogg@gmail.com>
2020-02-17 00:49:22 -0400

    Mostrando los Productos en pantalla

---
+++ src/componentes/Producto.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+const Producto = ({producto}) => {
+    return(
+        <tr>
+            <td>{producto.nombre}</td>
+            <td><span className="font-weight-bold">${producto.precio}</span></td>
+            <td className="acciones">
+                <Link to={`/productos/editar/${producto.id}`}
+                        className="btn btn-primary mr-2"
+                >
+
+                Editar
+                </Link>
+            </td>
+        </tr>
+    )
+}
+
+export default Producto;
--- src/componentes/Productos.js
+++ src/componentes/Productos.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import Producto from "./Producto"
 
 /* Redux */
 import { useDispatch, useSelector } from "react-redux";
@@ -16,26 +17,37 @@ const Productos = () => {
 
   /* Acceder al state */
   const loading = useSelector(state => state.productos.loading);
-  const error = useSelector( state => state.productos.error)
+  const error = useSelector(state => state.productos.error);
+  const productos = useSelector ( state => state.productos.productos)
   return (
     <React.Fragment>
-      {error ? <div className="font-weight-bold alert alert-danger text-center">Hubo un error...</div> 
-      :
-      <React.Fragment>
-      <h2 className="text-center my-5">Listado de Productos</h2>
-      <table className="table table-striped">
-        <thead className="bg-primary table-dark">
-          <tr>
-            <th scope="col">Nombre</th>
-            <th scope="col">Precio</th>
-            <th scope="col">Acciones</th>
-          </tr>
-        </thead>
-        <tbody></tbody>
-      </table>
-      {loading ? "Cargando..." : null}
-      </React.Fragment>
-      }
+      {error ? (
+        <div className="font-weight-bold alert alert-danger text-center">
+          Hubo un error...
+        </div>
+      ) : (
+        <React.Fragment>
+          <h2 className="text-center my-5">Listado de Productos</h2>
+          <table className="table table-striped">
+            <thead className="bg-primary table-dark">
+              <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {productos.map(producto => (
+                <Producto
+                  key={producto.id}
+                  producto={producto}
+                />
+              ))}
+            </tbody>
+          </table>
+          {loading ? "Cargando..." : null}
+        </React.Fragment>
+      )}
     </React.Fragment>
   );
 };
