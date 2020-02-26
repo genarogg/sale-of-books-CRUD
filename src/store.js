4db1a3f6dade2e5c1d1e135177640a646c52ae45
Genaro Gonzalez <genarrogg@gmail.com>
2020-02-25 22:53:12 -0400

    fix de dependencias

--- src/store.js
+++ src/store.js
@@ -1,16 +1,28 @@
-import { createStore, applyMiddleware, compose  } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleware = [thunk];
 
+/* Para desarrollo */
+/* const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+); */
+
+/* Para produccion  */
 const store = createStore(
-   /*  rootReducer,  */
-    /* initialState, */
-     compose(applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)  );
+   rootReducer,
+   initialState,
+   compose(
+     applyMiddleware(...middleware)
+   )
+ );
 
 export default store;
